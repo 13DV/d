14f19f1271398e04a6b1e6819a72c4b66d3ee39a
Olesya Alferova <mira.ponitkee@gmail.com>
2024-03-01 00:58:11 +0300

    03.01-2

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -1,12 +1,12 @@
-from django.contrib import admin
-from django.urls import path, include
+"""from django.contrib import admin"""
+from django.urls import path
 from . import views
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('posts/<int:id>/', views.post_detail, name='post_detail'),  # Detailed view of a post
-    path('category/<slug:category_slug>/', views.category_posts, name='category_posts'),  # Posts by category
-
+    path('posts/<int:id>/', views.post_detail, name='post_detail'),
+    path('category/<slug:category_slug>/', views.category_posts,
+         name='category_posts'),
 ]
